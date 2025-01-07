bf98eac8cf856db47724c85a696783f14bd8c413
Ivan <plagues.wtp@gmail.com>
2025-01-07 15:06:28 +0300

    merge and switch to Django

--- app/db/base.py
+++
@@ -1,7 +0,0 @@
-"""
-Import all models here so that Alembic or other migration tools can detect them.
-"""
-from app.db.base_class import Base  # noqa
-from app.models.user import User    # noqa
-from app.models.product import Product  # noqa
-from app.models.order import Order  # noqa
